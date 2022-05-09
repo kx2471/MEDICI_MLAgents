e255d0aaa9ea0d62e543829a934c00dd8a88d6df
Kim SangOk <han29181@naver.com>
2022-05-09 14:40:20 +0900

    유도로켓탄 실험

--- MummyML/Assets/Scripts/StageManager.cs
+++ MummyML/Assets/Scripts/StageManager.cs
@@ -5,45 +5,32 @@ using UnityEngine;
 public class StageManager : MonoBehaviour
 {
 
-    public GameObject goodItem;
+    
     public GameObject badItem;
 
-    [Range(10, 50)]
-    public int goodItemCount = 30;
+   
     [Range (10, 50)]
-    public int badItemCount = 20;
+    public int badItemCount = 40;
 
 
-    public List<GameObject> goodItemList = new List<GameObject> ();
+    
     public List<GameObject> badItemList = new List<GameObject> ();
 
     public void InitStage()
     {
-        foreach(var obj in goodItemList)
-        {
-            Destroy (obj);
-        }
-        goodItemList.Clear ();
-
+     
         foreach(var obj in badItemList)
         {
             Destroy(obj);
         }
         badItemList.Clear ();
 
-        for(int i = 0; i < goodItemCount; i++)
-        {
-            Vector3 pos = new Vector3
-                (Random.Range(-23.0f, 23.0f), 0.05f, Random.Range(-23.0f, 23.0f));
-            Quaternion rot = Quaternion.Euler(Vector3.up * Random.Range(0, 360));
-
-            goodItemList.Add(Instantiate(goodItem, transform.position + pos, rot, transform));
-        }
+      
 
         for (int i = 0; i < badItemCount; i++)
         {
             Vector3 pos = new Vector3
-                (Random.Range(-23.0f, 23.0f), 0.05f, Random.Range(-23.0f, 23.0f));
+                (Random.Range(-23.0f, 23.0f), Random.Range(0.05f, 35), Random.Range(-23.0f, 23.0f));
             Quaternion rot = Quaternion.Euler(Vector3.up * Random.Range(0, 360));
 
             badItemList.Add(Instantiate(badItem, transform.position + pos, rot, transform));
